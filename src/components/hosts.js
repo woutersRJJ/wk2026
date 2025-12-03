e8c5337bb5968ba813b904ca4dd7e6fe811315e6
Ronny Wouters <ozzy.wouters@telenet.be>
2025-12-03 20:50:45 +0100

    stadions voltalli+foto

--- src/components/hosts.js
+++ src/components/hosts.js
@@ -1,7 +1,21 @@
 import {firestoreDatabase} from "../services/firestore";
 import {useEffect, useState} from "react";
-import monterey from "../img/monterey.avif";
 import toronto from "../img/toronto.avif";
+import vancouver from "../img/vancouver.avif";
+import monterey from "../img/monterey.avif";
+import guadalajara from "../img/guadalajara.avif";
+import mexicoCity from "../img/mexicoCity.avif";
+import atlanta from "../img/atlanta.avif";
+import boston from "../img/boston.avif";
+import dallas from "../img/dallas.avif";
+import houston from "../img/houston.avif";
+import NYC from "../img/NYC.avif";
+import sanFran from "../img/sanFran.avif";
+import seattle from "../img/seattle.avif";
+import miami from "../img/miami.avif";
+import philadelphia from "../img/philadelphia.avif";
+import LA from "../img/LA.avif";
+import kansas from "../img/kansas.avif";
 
 export function Hosts() {
     const [hosts, setHosts] = useState([])
@@ -33,6 +47,7 @@ function Host({host}) {
         <div style={{marginTop: '10px'}}>{host.city}</div>
         <div>{host.stadium.name}</div>
         <div>Capacity: {host.stadium.capacity}</div>
+        <div>Opened: {host.stadium.opened}</div>
         <ImageComponent city={host.city}/>
     </div>
 }
@@ -43,6 +58,34 @@ function ImageComponent({city}){
             return <img src={monterey} alt={'foto'} width={'350px'}/>
         case 'Toronto':
             return <img src={toronto} alt={'foto'} width={'350px'}/>
+        case 'Vancouver':
+            return <img src={vancouver} alt={'foto'} width={'350px'}/>
+        case 'Guadalajara':
+            return <img src={guadalajara} alt={'foto'} width={'350px'}/>
+        case 'Mexico City':
+            return <img src={mexicoCity} alt={'foto'} width={'350px'}/>
+        case 'Atlanta':
+            return <img src={atlanta} alt={'foto'} width={'350px'}/>
+        case 'Boston':
+            return <img src={boston} alt={'foto'} width={'350px'}/>
+        case 'Dallas':
+            return <img src={dallas} alt={'foto'} width={'350px'}/>
+        case 'Houston':
+            return <img src={houston} alt={'foto'} width={'350px'}/>
+        case 'New York':
+            return <img src={NYC} alt={'foto'} width={'350px'}/>
+        case 'San Fransisco':
+            return <img src={sanFran} alt={'foto'} width={'350px'}/>
+        case 'Seattle':
+            return <img src={seattle} alt={'foto'} width={'350px'}/>
+        case 'Miami':
+            return <img src={miami} alt={'foto'} width={'350px'}/>
+        case 'Philadelphia':
+            return <img src={philadelphia} alt={'foto'} width={'350px'}/>
+        case 'Los Angeles':
+            return <img src={LA} alt={'foto'} width={'350px'}/>
+        case 'Kansas':
+            return <img src={kansas} alt={'foto'} width={'350px'}/>
         default: return null;
     }
 }
