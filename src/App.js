36cf80aa3ec966a31440d616af52d495c13e0517
Ronny Wouters <ozzy.wouters@telenet.be>
2025-12-03 16:46:52 +0100

    hosst cities canada-usa-mexico index voor firestore query met orderBy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './services/firestore'
 import './App.css';
 import {Banner} from "./components/Banner";
+import {Hosts} from "./components/hosts";
 
 function App() {
   return (
     <div className="App">
       <Banner/>
+        <Hosts/>
     </div>
   );
 }
---
+++ src/components/hosts.js
@@ -0,0 +1,38 @@
+import {firestoreDatabase} from "../services/firestore";
+import {useEffect, useState} from "react";
+
+export function Hosts(){
+    const [hosts,setHosts] = useState([])
+    useEffect(() => {
+        loadHosts();
+    }, []);
+
+    async function loadHosts() {
+        const result = await firestoreDatabase.collection('hosts').orderBy("country","asc").orderBy("city","asc").get();
+        setHosts(result.docs.map(doc => ({...doc.data()})));
+    }
+
+    return <div>
+        <h2>Hosts</h2>
+        {/*<button onClick={()=>{loadHosts()}}>Load</button>*/}
+        <div>
+            <h3>Canada</h3>
+            {hosts.filter(h=>h.country==='Canada').map((h,index)=><Host host={h} key={index} />)}
+            <h3>Mexico</h3>
+            {hosts.filter(h=>h.country==='Mexico').map((h,index)=><Host host={h} key={index} />)}
+            <h3>USA</h3>
+            {hosts.filter(h=>h.country==='USA').map((h,index)=><Host host={h} key={index} />)}
+        </div>
+
+    </div>
+}
+
+function Host({host}) {
+    return <div className='speler'>
+        <div style={{marginTop: '10px'}}>{host.city}</div>
+        {/*{<div>{host.country}</div>}*/}
+        <div>{host.stadium.name}</div>
+        <div>Capacity: {host.stadium.capacity}</div>
+    </div>
+
+}
